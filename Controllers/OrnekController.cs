b8f05bd9a171eedab00a99323a5d899b8d21f9ff
Betül Sarı <betulsari_97@hotmail.com>
2022-11-02 19:38:33 +0300

    redirectoAction method learned

--- Controllers/OrnekController.cs
+++ Controllers/OrnekController.cs
@@ -8,5 +8,27 @@ namespace AspNetCoreApp.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Index2()
+        {
+            // bu metot ile yönlendirme işlemi gerçekleştirebiliriz.
+            // Örneğin /Ornek/index2 yazıldığında yine de Ornek/index e gidiyor.
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+
+        public IActionResult JsonResult()
+        {
+            return Json(new { Id = 1, name = "kalem 1", price = 1 });
+        }
+
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
     }
 }
